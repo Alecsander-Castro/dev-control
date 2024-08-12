5b75ddeff15d4b9f6085351ea1a9f2c74b676605
Alecsander Castro <39681040+Alecsander-Castro@users.noreply.github.com>
2024-08-12 20:38:45 -0300

    feat: add more expects on Headet.test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,13 @@
-import { Header } from "./Header"
-import {render, screen} from "@testing-library/react"
+import { Header } from "./Header";
+import { render, screen } from "@testing-library/react";
 
-describe('<Header/>',() => {
-    it("should render <Header/> component", () => {
-       render(<Header/>)
-       const page = screen.getByTestId("header")
-       expect(page).toBeInTheDocument()
-    })
-} )
+describe("<Header/>", () => {
+  it("should render <Header/> component", () => {
+    render(<Header />);
+    const page = screen.getByTestId("header");
+    const logo = screen.getByTestId("logo");
+    expect(page).toBeInTheDocument();
+    expect(logo).toBeInTheDocument();
+    expect(logo).toHaveTextContent("DEVCONTROLE");
+  });
+});
